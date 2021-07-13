39edd5e088763b686524ce6d602f4b4c707d9745
Felipe Sug <54954812+felipesugisawa@users.noreply.github.com>
2021-07-13 10:32:32 -0300

    Add files via upload

---
+++ Contain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Learn //name project
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] phrase = { "Julio", "Cley", "Felipe", "Ana", "Gessica" };
+            foreach (string p in phrase)
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine("write an word for you search:");
+            var word = Console.ReadLine().ToLower();
+            var result = ""; 
+
+            if (phrase.Contains(word))
+            {
+                result = "Yes";
+            }
+            else
+            {
+                result = "No";
+            }
+                
+            Console.WriteLine("Contain?" + result);
+            Console.ReadLine();
+        }
+        //static void Main(string[] args)
+        //{
+        //    var phrase = "Hello world, I love the pogramming world".ToLower();
+        //    Console.WriteLine(phrase);
+        //    Console.WriteLine("write an word for you search:");
+        //    var word = Console.ReadLine().ToLower();
+        //    var result = phrase.IndexOf(word, phrase.IndexOf(word) + 1); //search second element 
+
+        //    Console.WriteLine("Location:" + result.ToString());
+        //    Console.ReadLine();
+        //}
+    }
+}
